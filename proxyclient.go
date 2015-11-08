a3867179f981abe4c105920e5f3cf905665c8157
GameXG <GameXG@Gmail.com>
2015-11-08 18:37:17 +0800

    增加文档

--- proxyclient.go
+++ proxyclient.go
@@ -16,6 +16,7 @@ type Conn interface {
 }
 
 // 表示 TCP 连接
+// 提供 net.TcpConn 全部的方法，但是部分方法由于代理协议的限制可能不能获得正确的结果。例如：LocalAddr 、RemoteAddr 方法不被很多代理协议支持。
 type TCPConn interface {
 	Conn
 
@@ -40,14 +41,24 @@ type UDPConn interface {
 	Conn
 }
 
-// 仿 net 库接口
+// 仿 net 库接口的代理客户端
+// 支持级联代理功能，可以通过 SetUpProxy 设置上级代理。
 type ProxyClient interface {
+	// 返回本代理的上层级联代理
 	UpProxy() ProxyClient
+	// 设置本代理的上层代理
 	SetUpProxy(upProxy ProxyClient) error
 
+	// Dial 在网络network上连接地址address，并返回一个Conn接口。可用的网络类型有：
+	// "tcp"、"tcp4"、"tcp6"、"udp"、"udp4"、"udp6"
+	// 对TCP和UDP网络，地址格式是host:port或[host]:port，参见函数JoinHostPort和SplitHostPort。
+	// 如果代理服务器支持远端DNS解析，那么会使用远端DNS解析。
 	Dial(network, address string) (Conn, error)
 	DialTimeout(network, address string, timeout time.Duration) (Conn, error)
+	// DialTCP在网络协议net上连接本地地址laddr和远端地址raddr。net必须是"tcp"、"tcp4"、"tcp6"；如果laddr不是nil，将使用它作为本地地址，否则自动选择一个本地地址。
+	// 由于 net.TCPAddr 内部保存的是IP地址及端口，所以使用本函数无法使用远端DNS解析，要想使用远端DNS解析，请使用 Dial 或 DialTCPSAddr 函数。
 	DialTCP(net string, laddr, raddr *net.TCPAddr) (TCPConn, error)
+	// DialTCPSAddr 同 DialTCP 函数，主要区别是如果代理支持远端dns解析，那么会使用远端dns解析。
 	DialTCPSAddr(network string, raddr string) (TCPConn, error)
 	//ListenTCP在本地TCP地址laddr上声明并返回一个*TCPListener，net参数必须是"tcp"、"tcp4"、"tcp6"，如果laddr的端口字段为0，函数将选择一个当前可用的端口，可以用Listener的Addr方法获得该端口。
 	//ListenTCP(net string, laddr *TCPAddr) (*TCPListener, error)
@@ -57,7 +68,8 @@ type ProxyClient interface {
 
 // 创建代理客户端
 // http 代理 http://123.123.123.123:8088
-// socks4 代理 socks4://123.123.123.123:5050  不支持远端 dns 解析
+// https 代理 https://123.123.123.123:8088
+// socks4 代理 socks4://123.123.123.123:5050  socks4 协议不支持远端 dns 解析
 // socks4a 代理 socks4a://123.123.123.123:5050
 // socks5 代理 socks5://123.123.123.123:5050?upProxy=http://145.2.1.3:8080
 // 直连 direct://0.0.0.0:0000/?LocalAddr=123.123.123.123:0
